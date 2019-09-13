f04f329b45d03b6c8fd487b7f260a8f57a342e8a
gjohnston1 <47825502+gjohnston1@users.noreply.github.com>
2019-09-13 12:44:47 -0400

    Update Take two Stones.py

--- Take two Stones.py
+++ Take two Stones.py
@@ -1,7 +1,11 @@
 Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 >>> amount_of_stones = int(input())
-
+# added this from someone's code from class when she went over it
+if not 1 <= N <= 10000000:
+        sys.exit()
+#this is the end of the added code from hers
+#Creates a boundary of the number allowed to be input, sys.exit closes the system when the number is input
 if amount_of_stones%2==0:
         print ("Bob")
 else:
