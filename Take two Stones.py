fc57a8c2f3d01907ab95ce4fb84604a8e4453e62
gjohnston1 <47825502+gjohnston1@users.noreply.github.com>
2019-09-13 12:41:46 -0400

    Add files via upload

---
+++ Take two Stones.py
@@ -0,0 +1,8 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 23:09:28) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> amount_of_stones = int(input())
+
+if amount_of_stones%2==0:
+        print ("Bob")
+else:
+        print ("Alice")
